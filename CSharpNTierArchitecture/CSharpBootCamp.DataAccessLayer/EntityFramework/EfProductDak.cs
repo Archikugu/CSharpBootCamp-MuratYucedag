a71b6af9dea34fb9b8a48cf72ec7253e677fc96f
Gokhan <engokhangok@gmail.com>
2024-12-26 00:07:01 +0300

    C# Bootcamp Lecture 18 - EntityState Commands, Generic Repository Class, and EF Classes

---
+++ CSharpNTierArchitecture/CSharpBootCamp.DataAccessLayer/EntityFramework/EfProductDak.cs
@@ -0,0 +1,15 @@
+﻿using CSharpBootCamp.DataAccessLayer.Abstract;
+using CSharpBootCamp.DataAccessLayer.Repositories;
+using CSharpBootCamp.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBootCamp.DataAccessLayer.EntityFramework
+{
+    public class EfProductDal : GenericRepository<Product>, IProductDal
+    {
+    }
+}
